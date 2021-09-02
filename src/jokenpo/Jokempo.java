8d3eaf8c607312ebbde4141ece8f6711bdc6e002
EDUARDO SANTOS NASCIMENTO <21276323@CFP127ALUNO.LOCAL>
2021-09-02 15:48:35 -0300

    ajustes

--- src/jokenpo/Jokempo.java
+++ src/jokenpo/Jokempo.java
@@ -8,6 +8,7 @@ public class Jokempo {
 	public static void main(String[] args) {
 
 		int jog, numc;
+		String num =" ", nump = " ";
 
 		System.out.println("******** Jokempo ********");
 		System.out.println("1 - pedra");
@@ -20,19 +21,63 @@ public class Jokempo {
 
 		Random com = new Random();
 		numc = com.nextInt(3) + 1;
-		System.out.println("O computador escolheu " + numc);
+		
+		if (numc == 1) {
+			
+			num = " Pedra";
+			
+		} else if (numc == 2) {
+			
+			num = " Papel";
+			
+		} else if (numc == 3) {
+			
+			num = " Tesoura";
+			
+		}else {
+			
+			System.out.println("erro");
+			
+		}
+		
+		if (jog == 1) {
+			
+			nump = " Pedra";
+			
+		} else if (jog == 2) {
+			
+			nump = " Papel";
+			
+		} else if (jog == 3) {
+			
+			nump = " Tesoura";
+			
+		}
+		
+
+		
 
 		if (numc == 1 && jog == 2 || numc == 2 && jog == 3 || numc == 3 && jog == 1) {
-
+			
+			System.out.println("O jogador escolheu" + nump );
+			System.out.println("O computador escolheu" + num );
 			System.out.println("******* YOU WIN ********");
 
 		} else if (numc == jog ) {
 			
+			System.out.println("O jogador escolheu" + nump );
+			System.out.println("O computador escolheu" + num );
 			System.out.println("******** Empate ********");
+			
+		} else if (jog == 1 && numc == 2 || jog == 2 && numc == 3 || jog == 3 && numc == 1) {
+			
+			System.out.println("O jogador escolheu" + nump );
+			System.out.println("O computador escolheu" + num );
+			System.out.println("****** GAME OVER *******");
 			
 		} else {
 			
-			System.out.println("****** GAME OVER *******");
+			System.out.println("Insira um valor valido!");
 			
 		}
 
